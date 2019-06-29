395e92f9ded490c824258cb1f7dc858f9dbffbed
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-29 18:46:19 +0300

    Контроллер WebAPI IProductData

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.DTO.Product;
+using WebStore.Domain.Entities.Products;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("api/products")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase, IProductData
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("sections")]
+        public IEnumerable<Section> GetSections()
+        {
+            return _ProductData.GetSections();
+        }
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _ProductData.GetBrands();
+        }
+
+        [HttpPost, ActionName("Post")]
+        public IEnumerable<ProductDTO> GetProducts([FromBody] ProductFilter Filter)
+        {
+            return _ProductData.GetProducts(Filter);
+        }
+
+        [HttpGet("{id}"), ActionName("Get")]
+        public ProductDTO GetProductById(int id)
+        {
+            return _ProductData.GetProductById(id);
+        }
+    }
+}
