4d75c64520ac2a7912265bcad7b177a82192ab52
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-07-10 20:07:52 +0300

    Тестирование HomeController

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -10,20 +10,23 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         //[ActionFilterAsync]
-        public IActionResult Index()
-        {
-            //throw new ApplicationException("Тестовое исключение");
-            return View();
-        }
+        public IActionResult Index() => View();
 
         public IActionResult ContactUs() => View();
-
-        public IActionResult Checkout() => View();
 
         public IActionResult Blog() => View();
 
         public IActionResult BlogSingle() => View();
 
         public IActionResult Error404() => View();
+
+        public IActionResult ErrorStatusCode(string id)
+        {
+            switch (id)
+            {
+                case "404": return RedirectToAction(nameof(Error404));
+                default: return Content($"Произошла ошибка с кодом {id}");
+            }
+        }
     }
 }
